7bab191b0622e0ddf41ec3494ef8f60af4d65b56
Adham Muhammad <admuaq@gmail.com>
2018-07-10 06:21:52 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 def assign_rooms(attendees)
   cont = []
   attendees.each_with_index do |element, i|
-    cont << "Hello #{element}! You'll be assigned to room #{i}!"
+    cont << "Hello, #{element}! You'll be assigned to room #{i+1}!"
   end 
   return cont
 end 
