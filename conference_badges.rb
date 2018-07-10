22da65026ad75c35b14748e79aa56dcd4a018cdb
Adham Muhammad <admuaq@gmail.com>
2018-07-10 05:55:47 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(name)
 end
   
 def batch_badge_creator(attendees)
-  container = attendees
+  container = []
   attendees.each do |badges|
     container << badge_maker(badges)
   end 
