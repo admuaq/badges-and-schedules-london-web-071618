40de3f214a499efce1c5c32e9c8cc25527f58b71
Adham Muhammad <admuaq@gmail.com>
2018-07-10 06:50:08 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,6 +20,7 @@ def assign_rooms(attendees)
 end 
 
 def printer(attendees)
-    puts batch_badge_creator(attendees).each
-    puts assign_rooms(attendees).each
+    batch_badge_creator(attendees).each do |pass|
+      puts pass 
+    assign_rooms(attendees).each do 
 end
